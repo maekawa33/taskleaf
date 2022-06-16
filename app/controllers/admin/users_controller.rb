b71ad20c4044bd5afa71f6d5d713a4a54d832528
Maekawa Kouki <u19b138@wako.ac.jp>
2022-06-16 22:35:05 +0900

    rspec

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :require_admin
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_06_12_100357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
-    t.index ["name"], name: "index_tasks_on_name", unique: true
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_06_12_100357) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", null: false
+    t.boolean "admin",default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
